41ad5039d7bab593d896f5d9f993d430091832db
Syed Imran <syedimran.dev1@gmail.com>
2023-01-10 23:13:01 +0500

    Manipulating the state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './components/person.js';
 
-function App() {
-  return (
+class App extends Component{
+  state = {
+    persons: [
+      {name: "Imran", age: "19"},
+      {name: "Faizan", age: "18"},
+      {name: "Sultan", age: "24"}
+    ]
+  }
+
+  SwitchNameHandler = () =>{
+    console.log('Clicked')
+    this.setState({
+      persons:[
+        {name: "Sultan", age: "24"},
+        {name: "Imran", age: "19"},
+        {name: "Faizan", age: "18"}
+      ]
+    })
+  }
+
+  render(){
+    return(
     <div className="App">
       <h1>Hi I'm a React App</h1>
+      <button onClick={this.SwitchNameHandler}>Switch</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ src/components/person.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const person = (props) => {
+  return (
+    <p>I'm {props.name} and my age is {props.age} </p>
+  )
+}
+
+export default person
